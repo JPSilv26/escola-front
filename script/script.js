00ba024c45149869fd7097a7e1bf1618b7838f63
JPSilv26 <100175325+JPSilv26@users.noreply.github.com>
2022-03-03 11:24:35 -0300

    att 03/03 11h24

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
-$('.banner').slick();
+$(`.banner`).slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    adaptiveHeight: true
+  });
